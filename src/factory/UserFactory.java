8fb7440762b0d3009a962c75cd2b3e61d4d425f1
user <230528@astanait.edu.kz>
2024-11-07 22:06:34 +0600

    Add UserFactory to handle dynamic creation of Salesman and Buyer roles

--- src/factory/UserFactory.java
+++ src/factory/UserFactory.java
@@ -1,4 +1,17 @@
 package factory;
 
+import models.Buyer;
+import models.Salesman;
+import models.User;
+
 public class UserFactory {
+    public User createUser(String name, String userType) {
+        if (userType.equalsIgnoreCase("Salesman")) {
+            return new Salesman(name);
+        } else if (userType.equalsIgnoreCase("Buyer")) {
+            return new Buyer(name);
+        } else {
+            return null;
+        }
+    }
 }
