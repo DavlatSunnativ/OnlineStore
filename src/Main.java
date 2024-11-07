54a061d4cf3a7d0d347b4d033f64bc4ddfd456fb
user <230528@astanait.edu.kz>
2024-11-07 23:18:56 +0600

    .

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import command.AddToCartCommand;
+import command.Cart;
+import command.RemoveFromCartCommand;
+import models.User;
+import observer.Observer;
+import observer.ProductNotifier;
+import factory.UserFactory;
+
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
