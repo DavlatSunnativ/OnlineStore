46f7c912299994a6248494e23559bbc8bb981246
user <230528@astanait.edu.kz>
2024-11-07 21:40:26 +0600

    Add OrderBuilder for flexible order creation with optional attributes

--- src/builder/OrderBuilder.java
+++ src/builder/OrderBuilder.java
@@ -1,4 +1,33 @@
 package builder;
 
+import models.Order;
+
 public class OrderBuilder {
+    private String productName;
+    private int quantity = 1; // Default quantity
+    private boolean giftWrap = false; // Default no gift wrap
+    private boolean discount = false; // Default no discount
+
+    public OrderBuilder(String productName) {
+        this.productName = productName;
+    }
+
+    public OrderBuilder setQuantity(int quantity) {
+        this.quantity = quantity;
+        return this;
+    }
+
+    public OrderBuilder setGiftWrap(boolean giftWrap) {
+        this.giftWrap = giftWrap;
+        return this;
+    }
+
+    public OrderBuilder setDiscount(boolean discount) {
+        this.discount = discount;
+        return this;
+    }
+
+    public Order build() {
+        return new Order(productName, quantity, giftWrap, discount);
+    }
 }
